1f45d810b85c0c5330f5c710c791eea1d820b289
NiklasNK-Creator <schoneniklas79@gmail.com>
2025-06-29 00:52:36 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,13 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from models import Base
+import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 Base = declarative_base()
 
 def create_tables():
+    from models import Base  # erst hier, um Zirkel zu verhindern
     Base.metadata.create_all(bind=engine)
