34ffa56dc878692dcafd80c10e5e3edb8d02bcf4
NiklasNK-Creator <schoneniklas79@gmail.com>
2025-06-29 18:40:47 +0200

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
+ffrom sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from database import Base
